e79bc349a3d06a713d2e75130b44654d85bd04db
AlirezaDaqiq <alireza.daghigh1999@gmail.com>
2021-10-14 19:17:18 +0330

    add ability to save users and their high score

--- 2048/game.py
+++ 2048/game.py
@@ -1,5 +1,33 @@
 import logic
+import User
+import pickle
 
+PIK = "users.pickle"
+
+
+def load_users(username):
+    with open(PIK, "rb") as f:
+        users = pickle.load(f)
+        for user in users:
+            if user.name == username:
+                return user
+    return None
+
+
+def save_user(user, new_user=True):
+    users = pickle.load(open(PIK, "rb"))
+    if new_user:
+        users.append(user)
+    else:
+        for i in range(len(users)):
+            if users[i].name == user.name:
+                users[i] = user
+    pickle.dump(users, open(PIK, "wb"))
+
+
+def initial_save():
+    data = []
+    pickle.dump(data, open(PIK, "wb"))
 
 def print_mat(mat):
     for i in range(logic.dimension):
@@ -9,37 +37,58 @@ def print_mat(mat):
 
 
 def runner():
+    new_user = False
+    name = input("enter your name: ")
+    user = load_users(name)
+    if user is None:
+        user = User.User(name)
+        new_user = True
     mat = logic.start_game()
     print_mat(mat)
+
     while True:
         cmd = input("Press the command : ")
         if cmd == 'w':
             mat = logic.move_up(mat)
             status = logic.game_state(mat)
             print("score : ", logic.score)
+            print("high score : ", user.best_score)
             if status == 'Continue':
-                print(status)
                 logic.add_random_element(mat)
             else:
+                if user.best_score < logic.score:
+                    user.best_score = logic.score
+                save_user(user, new_user)
+                print(status)
                 break
 
         elif cmd == 's':
             mat = logic.move_down(mat)
             status = logic.game_state(mat)
             print("score : ", logic.score)
+            print("high score : ", user.best_score)
             if status == 'Continue':
-                print(status)
                 logic.add_random_element(mat)
             else:
+                if user.best_score < logic.score:
+                    user.best_score = logic.score
+                save_user(user, new_user)
+                print(status)
+
                 break
 
         elif cmd == 'a':
             mat = logic.move_left(mat)
             status = logic.game_state(mat)
             print("score : ", logic.score)
+            print("high score : ", user.best_score)
+
             if status == 'Continue':
                 logic.add_random_element(mat)
             else:
+                if user.best_score < logic.score:
+                    user.best_score = logic.score
+                save_user(user, new_user)
                 print(status)
                 break
 
@@ -47,10 +96,15 @@ def runner():
             mat = logic.move_right(mat)
             status = logic.game_state(mat)
             print("score : ", logic.score)
+            print("high score : ", user.best_score)
+
             if status == 'Continue':
                 logic.add_random_element(mat)
             else:
                 print(status)
+                if user.best_score < logic.score:
+                    user.best_score = logic.score
+                save_user(user, new_user)
                 break
         else:
             print("Invalid Key Pressed")
@@ -60,4 +114,3 @@ def runner():
 
 if __name__ == "__main__":
     runner()
-
